cdbdfad35a281fb96616301fd8ba12d90ebdaf3f
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2025-05-02 21:04:34 +0530

    Update BottomBar.js

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function BottomBar() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white text-center py-3 z-50">
+    // <footer className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white text-center py-3 z-50">
+    <footer className="w-full bg-gray-800 text-white text-right pr-10 py-3 z-50">
       <p>&copy; {new Date().getFullYear()} LegendByte.com. All rights reserved.</p>
     </footer>
   );
